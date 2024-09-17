39ac5a23bc02e60b2441297b80d6e2c2f1fe527a
Michael <156242839+mochamochaccino@users.noreply.github.com>
2024-09-17 16:11:41 -0500

    created local state

--- client/src/jobdisplay/components/JobAccordion.tsx
+++ client/src/jobdisplay/components/JobAccordion.tsx
@@ -1,8 +1,7 @@
 import type { data } from "./JobDisplay";
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
+import { useState, useEffect } from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Accordion, AccordionSummary, AccordionDetails, TextField } from "@mui/material";
 
 
 interface props {
@@ -10,7 +9,27 @@ interface props {
 }
 
 export default function JobAccordion(props: props) {
+    const [company, setCompany] = useState<string>('');
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const [salary, setSalary] = useState<number>(0);
+    const [status, setStatus] = useState<string>('');
+    const [date, setDate] = useState<string>('');
+    const [industry, setIndustry] = useState<string>('');
+    const [subindustry, setSubIndustry] = useState<string>('');
     const { data } = props;
+
+    useEffect(() => {
+        setCompany(data.company);
+        setTitle(data.jobTitle);
+        setDescription(data.description);
+        setSalary(data.salary);
+        setStatus(data.status);
+        setDate(data.date);
+        setIndustry(data.industry);
+        setSubIndustry(data.subIndustry);
+    }, [company, title, description, salary, status, date, industry, subindustry])
+
     return(
         <div>
             <Accordion>
@@ -19,11 +38,40 @@ export default function JobAccordion(props: props) {
                 aria-controls="panel1-content"
                 id="panel1-header"
                 >
-                { data.company }
+                { company }
                 </AccordionSummary>
                 <AccordionDetails>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    <span>
+                        <TextField id="filled-basic" label="Company" variant="filled" value={company} />
+                    </span>
+                    <br/>
+                    <span>
+                        <TextField id="filled-basic" label="Job Title" variant="filled" value={title} />
+                    </span>
+                    <br/>
+                    <span>
+                        <TextField id="filled-basic" label="Description" variant="filled" value={description} />
+                    </span>
+                    <br/>
+                    <span>
+                        <TextField id="filled-basic" label="Salary" variant="filled" value={salary} />
+                    </span>
+                    <br/>
+                    <span>
+                        <TextField id="filled-basic" label="Status" variant="filled" value={status} />
+                    </span>
+                    <br/>
+                    <span>
+                        <TextField id="filled-basic" label="Date" variant="filled" value={date} />
+                    </span>
+                    <br/>
+                    <span>
+                        <TextField id="filled-basic" label="Industry" variant="filled" value={industry} />
+                    </span>
+                    <br/>
+                    <span>
+                        <TextField id="filled-basic" label="SubIndustry" variant="filled" value={subindustry} />
+                    </span>
                 </AccordionDetails>
             </Accordion>
         </div>
