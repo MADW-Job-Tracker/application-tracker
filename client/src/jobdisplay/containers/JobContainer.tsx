26b67e05a3d06b13f75e4f135a6fb8bf762a3ff4
William-Jack Dalessandro <william.dalessandro123@gmail.com>
2024-09-18 16:59:14 -0400

    some styling changes

--- client/src/jobdisplay/containers/JobContainer.tsx
+++ client/src/jobdisplay/containers/JobContainer.tsx
@@ -1,10 +1,12 @@
 import JobDisplay from "../components/JobDisplay"
+import { Typography, TextField, Box, Button, MenuItem, Select, InputLabel, FormControl, makeStyles } from '@mui/material';
 
 export default function JobContainer() {
-    return(
-        <div className="jobContainer">
-            <JobDisplay/>
-        </div>
-        
+    return (
+        <Box
+        sx={{ width: "65%" }}
+        >
+            <JobDisplay />
+        </Box>
     )
 }
